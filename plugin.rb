f1582d5e73dee3b335d683d5eb56b76fe0539849
Orlando Del Aguila <orlando@hashlabs.com>
2020-01-23 16:46:05 -0600

    dev: add CSP extension for Sentry

--- plugin.rb
+++ plugin.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # name: discourse-sentry
 # about: Discourse plugin to integrate Sentry (sentry.io)
 # version: 1.1
@@ -26,5 +27,9 @@ after_initialize do
         Raven.extra_context(params: params.to_unsafe_h, url: request.url)
       end
     end
+
+    extend_content_security_policy(
+      script_src: ['https://browser.sentry-cdn.com/5.11.1/bundle.min.js']
+    )
   end
 end
